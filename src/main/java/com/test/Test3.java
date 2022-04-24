1280f5cb67bf3598ed99b69f617d575a25e2a056
HPME <443808626@qq.com>
2022-04-24 16:25:54 +0800

    1

--- src/main/java/com/test/Test3.java
+++ src/main/java/com/test/Test3.java
@@ -1,7 +1,9 @@
 package com.test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * untitled：
@@ -11,10 +13,7 @@ import java.util.LinkedList;
  * @date 2022~04~18~14:55
  */
 public class Test3 {
-    public static void main(String[] args) {
-        Test3 test3 = new Test3();
-        System.out.println(Arrays.toString(test3.plusOne(new int[]{9,9,9})));
-    }
+
     public int[] plusOne(int[] digits) {
         boolean j=true;
         LinkedList<Integer> list=new LinkedList<>();
@@ -39,5 +38,52 @@ public class Test3 {
             x[i]=list.pop();
         }
         return x;
+    }
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> list=new ArrayList<>();
+        StringBuilder stringBuilder=new StringBuilder();
+        int sum,i=0,t,ty,js;
+        boolean p=true;
+        while(i<words.length&&p){
+            sum=0;
+            js=0;
+            while(sum-1<maxWidth&&i<words.length){
+                sum+=words[i].length()+1;
+                js++;
+                i++;
+            }
+            sum-=words[i-1].length()+js;
+            if (js-2!=0){
+                t=(maxWidth-sum)/(js-2);
+                ty=(maxWidth-sum)%(js-2);
+                int j;
+                for (j = i-js; j < i-2; j++) {
+                    stringBuilder.append(words[j]);
+                    if (ty>0){
+                        stringBuilder.append(" ");
+                        ty--;
+                    }
+                    stringBuilder.append(" ".repeat(Math.max(0, t)));
+                }
+                stringBuilder.append(words[j]);
+            }else {
+                stringBuilder.append(words[i-2]);
+            }
+            if (list.size()>0&&list.get(list.size()-1).trim().equals(stringBuilder.toString())){
+                break;
+            }
+            if (stringBuilder.length()<maxWidth){
+                js=maxWidth-stringBuilder.length();
+                stringBuilder.append(" ".repeat(Math.max(0, js)));
+            }
+            list.add(stringBuilder.toString());
+            stringBuilder.delete(0,stringBuilder.length());
+            i--;
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        Test3 test3 = new Test3();
+        System.out.println(test3.fullJustify(new String[]{"What","must","be","acknowledgment","shall","be"},16));
     }
 }
