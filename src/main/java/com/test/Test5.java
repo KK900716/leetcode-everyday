cc06500a315865a2d76f1dfbfff5137689cfb83d
HPME <443808626@qq.com>
2022-05-31 01:39:41 +0800

    1

--- src/main/java/com/test/Test5.java
+++ src/main/java/com/test/Test5.java
@@ -1,6 +1,7 @@
 package com.test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * untitled：
@@ -16,7 +17,11 @@ public class Test5 {
      * @param args args
      */
     public static void main(String[] args) {
-
+        Solution solution=new Solution();
+        int[][] ints = solution.generateMatrix(5);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(Arrays.toString(ints[i]));
+        }
     }
 }
 /**
@@ -26,8 +31,66 @@ public class Test5 {
  * @date 2022~05~30~22:40
  */
 class Solution {
+
+    /**
+     * com.test.Solution.generateMatrix():
+     *  给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+     * @author 44380
+     * @date 2022/5/30~23:45
+     * @param n 正整数n
+     * @return int[][] 正方形矩阵
+     */
+    public int[][] generateMatrix(int n) {
+        if (n==1){
+            return new int[][]{{1}};
+        }
+        int[][] res=new int[n][n];
+        int x=1;
+        int h=n,v=n-2;
+        int i=0;
+        int j=0;
+        int k;
+        while (true){
+            if (res[i][j]!=0){
+                return res;
+            }
+            for (k = 0; k < h; k++) {
+                res[i][j++]=x++;
+            }
+            j--;
+            i++;
+
+            if (res[i][j]!=0){
+                return res;
+            }
+            for (k = 0; k < v; k++) {
+                res[i++][j]=x++;
+            }
+
+            if (res[i][j]!=0){
+                return res;
+            }
+            for (k = 0; k < h; k++) {
+                res[i][j--]=x++;
+            }
+            j++;
+            i--;
+            h-=2;
+
+            if (res[i][j]!=0){
+                return res;
+            }
+            for (k = 0; k < v; k++) {
+                res[i--][j]=x++;
+            }
+            j++;
+            i++;
+            v-=2;
+        }
+    }
     /**
      * com.test.Solution.rotateRight():
+     * 链表右移
      * @author 44380
      * @date 2022/5/30~22:48
      * @param head 头结点
